5a74092b858171fe8f495a435176578964a6d46e
Bram Vogelaar <attachmentgenie@inuits.eu>
2015-01-27 15:59:35 +0100

    removing tmp files

--- virt/docker/rs1conf.js
+++
@@ -1,6 +0,0 @@
-rs.add("172.17.0.155:27017")
-rs.add("172.17.0.156:27017")
-cfg = rs.conf()
-cfg.members[0].host = "172.17.0.154:27017"
-rs.reconfig(cfg)
-rs.status()
--- virt/docker/sh.js
+++
@@ -1,3 +0,0 @@
-sh.addShard("rs1/172.17.0.154:27017")
-sh.addShard("rs2/172.17.0.157:27017")
-sh.status()
