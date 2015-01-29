3f837cebd0e5a5e3ab28e618d198ce88be94ab77
Bram Vogelaar <attachmentgenie@inuits.eu>
2015-01-29 13:08:31 +0100

    removing tmp files

--- testbench/bin/rs.js
+++
@@ -1,6 +0,0 @@
-  config = {_id: 'rs', members: [
-  {_id: 0, host: ''},
-  {_id: 1, host: ''},
-  {_id: 2, host: ''}]
-}
-rs.initiate(config);
--- testbench/bin/rs1.js
+++
@@ -1,6 +0,0 @@
-  config = {_id: 'rs1', members: [
-  {_id: 0, host: '172.17.0.37'},
-  {_id: 1, host: '172.17.0.38'},
-  {_id: 2, host: '172.17.0.39'}]
-}
-rs.initiate(config);
